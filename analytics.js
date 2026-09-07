b15f957b3e84cf7bb1c5948aa14ef5b41acc4f58
w-score <e.ikegaya@gmail.com>
2026-09-07 17:27:57 +0900

    アナリティクス設定

--- analytics.js
+++ analytics.js
@@ -1,7 +1,7 @@
 /* アクセス解析（Google アナリティクス GA4）
    Google アナリティクスで発行される測定ID（G-XXXXXXXXXX）を下の GA_ID に書き込むと、
    全ページで計測が始まります。空のままなら何もしません。 */
-var GA_ID = "";
+var GA_ID = "G-CLNR9WGSGN";
 if (GA_ID) {
   var s = document.createElement("script");
   s.async = true;
